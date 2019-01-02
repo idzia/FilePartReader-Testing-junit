e4fd3859c9d132eadc87f844cd9ecb39c2599a82
idzia <ania.idzi@gmail.com>
2019-01-02 11:00:50 +0100

    Add Mock to FileWordAnalyzerTest

--- src/test/com/codecool/java/FileWordAnalyzerTest.java
+++ src/test/com/codecool/java/FileWordAnalyzerTest.java
@@ -9,18 +9,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileWordAnalyzerTest {
 
+    @Mock
+    private FilePartReader filePartReader;
+
+    @BeforeEach
+    public void setUp() {
+        filePartReader = mock(FilePartReader.class);
+    }
+
+    @Test
+    public void testWordsByABC() throws IOException {
+
+        ArrayList<String> expectedList = new ArrayList<>();
+        expectedList.add("abc");
+        expectedList.add("def");
+        expectedList.add("xyz");
+
+        FileWordAnalyzer fileWord = new FileWordAnalyzer(filePartReader);
+        when(filePartReader.readLines()).thenReturn("def xyz abc ");
+        ArrayList<String> words = fileWord.wordsByABC();
+
+        assertEquals(expectedList.get(0), words.get(0));
+        assertEquals(expectedList.get(1), words.get(1));
+        assertEquals(expectedList.get(2), words.get(2));
+    }
+
+
+
+
+
+
     @Test
     public void testWordsByABCAscending() throws IOException {
         FileWordAnalyzer fileWord = new FileWordAnalyzer(new FilePartReader());
-        assertEquals("1a1", fileWord.wordsByABC().get(0));
-        assertEquals("7g", fileWord.wordsByABC().get(fileWord.wordsByABC().size()-1));
+        List<String> actual = fileWord.wordsByABC();
+        assertEquals("1a1", actual.get(0));
+        assertEquals("7g", actual.get(actual.size()-1));
     }
 
     @Test
     public void testWordsContainingSubStringCaseSensitive() throws IOException {
         FileWordAnalyzer fileWord = new FileWordAnalyzer(new FilePartReader());
         ArrayList<String> wordsList = fileWord.wordsContainingSubString("BB");
-        //assertEquals(2, wordsList.size());
         assertEquals("4bb4", wordsList.get(0));
         assertEquals("6bb", wordsList.get(1));
     }
@@ -38,7 +68,6 @@ class FileWordAnalyzerTest {
         FileWordAnalyzer fileWord = new FileWordAnalyzer(new FilePartReader());
         ArrayList<String> wordsList = fileWord.wordsContainingSubString("2ax");
         assertNotNull(wordsList);
-        //assertEquals(0, wordsList.size());
     }
 
 
